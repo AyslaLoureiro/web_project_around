540d190e06d4143b1f44128f34f132d2c4b151b7
Aysla <ayslaloureiro@gmail.com>
2024-09-02 22:32:56 +0100

    add function to add avatar

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,16 +1,19 @@
 export default class UserInfo {
-  constructor({ userName, userJob }) {
+  constructor({ userName, userJob, userAvatar }) {
     this._nameElement = document.querySelector(userName);
     this._jobElement = document.querySelector(userJob);
+    this._avatar = document.querySelector(userAvatar);
   }
   getUserInfo() {
     return {
       name: this._nameElement.textContent,
       job: this._jobElement.textContent,
+      avatar: this._avatar.getAttribute("src"),
     };
   }
-  setUserInfo({ name, job }) {
+  setUserInfo({ name, job, avatar }) {
     this._nameElement.textContent = name;
     this._jobElement.textContent = job;
+    this._avatar.setAttribute("src", avatar);
   }
 }
