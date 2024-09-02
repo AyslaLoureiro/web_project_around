3f56f7df7119c2f82f95cb341241cd5d0ce4ab1e
Aysla <ayslaloureiro@gmail.com>
2024-09-02 22:34:54 +0100

    implement new functions and instantiated new classes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,39 @@ import "./pages/index.css";
 import Card from "./components/Cards.js";
 import FormValidator from "./components/FormValidator.js";
 import Section from "./components/Section.js";
-import PopupWhitForm from "./components/PopupWithForm.js";
+import PopupWithConfirmation from "./components/PopupWithConfirmation.js";
 import PopupWithForm from "./components/PopupWithForm.js";
 import UserInfo from "./components/UserInfo.js";
 import PopupWithImage from "./components/PopupWithImage";
+import Api from "./components/Api.js";
+const api = new Api({
+  baseUrl: "https://around.nomoreparties.co/v1/web-ptbr-cohort-12",
+  headers: {
+    authorization: "7c4842cf-8417-4f42-a788-66302b6ea108",
+    "Content-Type": "application/json",
+  },
+});
+
+// Seleciona os formulários e configurações de validação
+const formElement = document.querySelector(".popup__form-title");
+const formElementAdd = document.querySelector(".popup__add-card-form");
+const formElementAvatar = document.querySelector(".popup__form-photo-edit");
+const config = {
+  form: ".popup__form-title",
+  input: ".popup__form-input",
+  submitButton: ".button",
+  buttonDisabledClass: "button__disabled",
+  errorClass: "popup__span-error",
+  inputErrorClass: "popup__form-input-invalid",
+};
+
+// Cria e habilita os validadores de formulário
+const formValidatorProfile = new FormValidator(config, formElement);
+const formValidatorAdd = new FormValidator(config, formElementAdd);
+const formValidatorAvatar = new FormValidator(config, formElementAvatar);
+formValidatorProfile.enableValidation();
+formValidatorAdd.enableValidation();
+formValidatorAvatar.enableValidation();
 
 // Seleciona o popup e adiciona um evento para fechá-lo ao clicar na área de sobreposição
 const popup = document.querySelector(".popup");
@@ -38,33 +67,24 @@ popupImage.addEventListener("click", (event) => {
   }
 });
 
-// Array inicial de cartões com nome e link de imagem
-const initialCards = [
-  {
-    name: "Vale de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
-  },
-  {
-    name: "Montanhas Carecas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
-  },
-  {
-    name: "Parque Nacional da Vanoise ",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
-  },
-];
+const editAvatar = document.querySelector(".profile__edit-icon");
+const popupAvatar = document.querySelector(".popup-avatar-edit");
+popupAvatar.addEventListener("click", (event) => {
+  if (event.target.classList.contains("overlay")) {
+    popupAvatar.classList.remove("popup__open");
+  }
+});
+const popupConfirmation = new PopupWithConfirmation(
+  ".popup-delete",
+  (card, cardId) => {
+    return api.deleteCard(cardId).then(() => {
+      card.remove();
+    });
+  }
+);
+popupConfirmation.setEventListeners();
+
+const initialCards = api.getInitialCards();
 
 const popupWithImage = new PopupWithImage(".popup-image");
 const handleCardClick = (link, title) => {
@@ -73,19 +93,30 @@ const handleCardClick = (link, title) => {
 
 popupWithImage.setEventListeners();
 
-// Cria e renderiza os cartões iniciais
-initialCards.forEach((card) => {
+const renderNewCard = (card) => {
   const cardElement = new Card(
     card.name,
     card.link,
     "#cards",
-    handleCardClick
+    handleCardClick,
+    card,
+    userData._id,
+    api.addLike.bind(api),
+    api.removeLike.bind(api),
+    () => {
+      popupConfirmation.open(cardElement, card._id);
+    }
   ).generateCard();
+
+  return cardElement;
+};
+
+initialCards.then((cards) => {
   const section = new Section(
     {
-      items: [cardElement],
+      items: cards,
       renderer: (cardParams) => {
-        section.addItem(cardParams);
+        section.addItem(renderNewCard(cardParams));
       },
     },
     ".elements"
@@ -96,13 +127,31 @@ initialCards.forEach((card) => {
 const userInfo = new UserInfo({
   userName: ".profile__title",
   userJob: ".profile__explorar",
+  userAvatar: ".profile__image",
 });
-const handleSubmitProfileForm = ({ name, about }) => {
-  userInfo.setUserInfo({ name, job: about });
+
+let userData = {};
+
+api.getUserInfo().then((userInformation) => {
+  userData = userInformation;
+  userInfo.setUserInfo({
+    name: userInformation.name,
+    job: userInformation.about,
+    avatar: userInformation.avatar,
+  });
+});
+
+const handleSubmitProfileForm = ({ name, about }, buttonSubmit) => {
+  buttonSubmit.textContent = "Salvando...";
+  api.editUserInfo({ name, about }).then(() => {
+    buttonSubmit.textContent = "Salvar";
+    formValidatorProfile.enableValidation();
+  });
+  userInfo.setUserInfo({ name, job: about, avatar: userData.avatar });
 };
 
 // Cria e renderiza o popup de edição de perfil
-const popupEditProfile = new PopupWhitForm(
+const popupEditProfile = new PopupWithForm(
   handleSubmitProfileForm,
   ".popup-edit-profile"
 );
@@ -113,23 +162,56 @@ edtButton.addEventListener("click", () => {
   popupEditProfile.open();
 });
 
-const handleAddCard = ({ title, link }) => {
-  const cardElement = new Card(
-    title,
-    link,
-    "#cards",
-    handleCardClick
-  ).generateCard();
-  const section = new Section(
-    {
-      items: [cardElement],
-      renderer: (card) => {
-        section.addOneItem(card);
+const handleSubmitAvatarForm = ({ avatar }, buttonSubmit) => {
+  buttonSubmit.textContent = "Salvando...";
+  api.editUserPhoto({ avatar }).then(() => {
+    buttonSubmit.textContent = "Salvar";
+    formValidatorAvatar.enableValidation();
+  });
+  userInfo.setUserInfo({ name: userData.name, job: userData.about, avatar });
+};
+
+const popupEditAvatar = new PopupWithForm(
+  handleSubmitAvatarForm,
+  ".popup-avatar-edit"
+);
+popupEditAvatar.setEventListeners();
+
+editAvatar.addEventListener("click", () => {
+  popupEditAvatar.open();
+});
+
+const handleAddCard = ({ title, link }, buttonSubmit) => {
+  buttonSubmit.textContent = "Criando...";
+  api.addNewCard({ name: title, link }).then((card) => {
+    const cardElement = new Card(
+      title,
+      link,
+      "#cards",
+      handleCardClick,
+      card,
+      userData._id,
+      api.addLike.bind(api),
+      api.removeLike.bind(api),
+      () => {
+        popupConfirmation.open(cardElement, card._id);
+      }
+    ).generateCard();
+
+    const section = new Section(
+      {
+        items: [cardElement],
+        renderer: (card) => {
+          section.addOneItem(card);
+        },
       },
-    },
-    ".elements"
-  );
-  section.renderer();
+      ".elements"
+    );
+    section.renderer();
+
+    buttonSubmit.textContent = "Criar";
+    formValidatorAdd.enableValidation();
+  });
 };
 
 const popupAddCard = new PopupWithForm(handleAddCard, ".popup-add");
@@ -149,21 +231,3 @@ document.onkeydown = function (event) {
     popupImage.classList.remove("popup__open");
   }
 };
-
-// Seleciona os formulários e configurações de validação
-const formElement = document.querySelector(".popup__form-title");
-const formElementAdd = document.querySelector(".popup__add-card-form");
-const config = {
-  form: ".popup__form-title",
-  input: ".popup__form-input",
-  submitButton: ".button",
-  buttonDisabledClass: "button__disabled",
-  errorClass: "popup__span-error",
-  inputErrorClass: "popup__form-input-invalid",
-};
-
-// Cria e habilita os validadores de formulário
-const formValidatorProfile = new FormValidator(config, formElement);
-const formValidatorAdd = new FormValidator(config, formElementAdd);
-formValidatorProfile.enableValidation();
-formValidatorAdd.enableValidation();
