15a27013772ffbc38a7aaffcdcd74fcb778726d8
Aysla <ayslaloureiro@gmail.com>
2024-09-02 22:33:58 +0100

    implement new card functions

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,11 +2,32 @@ import blackHeart from "../images/blackheart.svg";
 import heart from "../images/heart.svg";
 
 export default class Card {
-  constructor(title, link, cardTemplate, handleCardClick) {
+  constructor(
+    title,
+    link,
+    cardTemplate,
+    handleCardClick,
+    cardData,
+    ownerId,
+    handleAddLike,
+    handleRemoveLike,
+    openPopupConfirmation
+  ) {
     this._title = title;
     this._link = link;
     this._cardTemplate = cardTemplate;
     this._handleCardClick = handleCardClick;
+    this._cardData = cardData;
+    this._cardId = cardData._id;
+    this._handleAddLike = handleAddLike;
+    this._handleRemoveLike = handleRemoveLike;
+    this.cardElement = this._getTemplate();
+    this._countLike = this.cardElement.querySelector(".elements__count-like");
+    this._cardOwnerId = this._cardData.owner._id;
+    this._userId = ownerId;
+    this._likeButtonElement =
+      this.cardElement.querySelector(".elements__heart");
+    this._openPopupConfirmation = openPopupConfirmation;
   }
 
   _getTemplate() {
@@ -19,15 +40,31 @@ export default class Card {
   }
 
   generateCard() {
-    this.cardElement = this._getTemplate();
     this._setEventListeners();
-
     this.cardElement.querySelector(".elements__image").src = this._link;
     this.cardElement.querySelector(
       ".elements__image"
     ).alt = `imagem do ${this._title}`;
     this.cardElement.querySelector(".elements__title").textContent =
       this._title;
+
+    this._countLike.textContent = this._cardData.likes.length;
+
+    this._cardData.likes.forEach((likeObj) => {
+      if (this._userId === likeObj._id) {
+        return this._likeButtonElement.setAttribute("src", blackHeart);
+      } else {
+        return this._likeButtonElement.setAttribute("src", heart);
+      }
+    });
+
+    if (this._userId === this._cardOwnerId) {
+      this.cardElement.querySelector(".elements__trash-button").style.display =
+        "block";
+    } else {
+      this.cardElement.querySelector(".elements__trash-button").style.display =
+        "none";
+    }
 
     return this.cardElement;
   }
@@ -38,9 +75,16 @@ export default class Card {
       .addEventListener("click", (e) => {
         const likeButton = e.target;
         if (likeButton.getAttribute("src") === blackHeart) {
+          const response = this._handleRemoveLike(this._cardId);
+          response.then((card) => {
+            this._countLike.textContent = card.likes.length;
+          });
           return likeButton.setAttribute("src", heart);
         }
-
+        const response = this._handleAddLike(this._cardId);
+        response.then((card) => {
+          this._countLike.textContent = card.likes.length;
+        });
         return likeButton.setAttribute("src", blackHeart);
       });
   }
@@ -48,10 +92,8 @@ export default class Card {
   _trashButton() {
     this.cardElement
       .querySelector(".elements__trash-button")
-      .addEventListener("click", (e) => {
-        const card = e.target.closest(".elements__item");
-
-        card.remove();
+      .addEventListener("click", () => {
+        this._openPopupConfirmation();
       });
   }
 
